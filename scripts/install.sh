b0c63e66053317f74287ef680530849f2594ad55
周游 <hi@zhouyou.info>
2025-12-19 00:22:02 +0800

    优化安装脚本，添加SHA256校验以确保下载的NVIDIA驱动文件的完整性

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,18 +1,32 @@
 #!/usr/bin/env bash
 
 version="25.10.1"
-
-# disable nvidia support temporarily
-midclt call docker.update '{"nvidia": false}'
+sha256="ad25bd62828cb7724ee5b7479a0a4a2e673c891259ce295e20b05ce4027dd48e"
 
 # download nvidia raw file
 wget -O /tmp/nvidia.raw "https://github.com/zzzhouuu/truenas-nvidia-drivers/raw/refs/heads/main/${version}/nvidia.raw"
 
-# install nvidia raw file
-zfs set readonly=off "$(zfs list -H -o name /usr)"
-mv /usr/share/truenas/sysext-extensions/nvidia.raw /usr/share/truenas/sysext-extensions/nvidia.raw.bak
-mv /tmp/nvidia.raw /usr/share/truenas/sysext-extensions/
-zfs set readonly=on "$(zfs list -H -o name /usr)"
+# verify the downloaded file
+if echo "${sha256}  /tmp/nvidia.raw" | sha256sum -c -; then
+  echo "SHA256 checksum verification passed."
 
-# enable nvidia support
-midclt call docker.update '{"nvidia": true}'
+  # disable nvidia support temporarily
+  midclt call docker.update '{"nvidia": false}'
+
+  # install nvidia raw file
+  zfs set readonly=off "$(zfs list -H -o name /usr)"
+  mv /usr/share/truenas/sysext-extensions/nvidia.raw /usr/share/truenas/sysext-extensions/nvidia.raw.bak
+  mv /tmp/nvidia.raw /usr/share/truenas/sysext-extensions/
+  zfs set readonly=on "$(zfs list -H -o name /usr)"
+
+  # enable nvidia support
+  midclt call docker.update '{"nvidia": true}'
+
+  echo "NVIDIA drivers installed successfully."
+else
+  echo "SHA256 checksum verification failed!"
+  echo "Deleting invalid files..."
+
+  rm -f /tmp/nvidia.raw
+  exit 1
+fi
